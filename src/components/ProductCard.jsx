cb384fcb99c7b4b9b89f0cca856e35fd28400ac9
여다희 <yeodahui.dev@gmail.com>
2022-09-14 17:57:12 +0900

    추가 : 프로젝트 카드 컴포넌트 생성

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+// import TagArray from "./TagArray";
+
+const ProductCard = ({ image, alt, title, description, tags }) => (
+  <Card sx={{ maxWidth: 345 }}>
+    <CardMedia component="img" alt={alt} height="140" image={image} />
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="div">
+        {title}
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        {description}
+      </Typography>
+    </CardContent>
+    {/* <TagArray tags={tags} /> */}
+  </Card>
+);
+
+ProductCard.defaultProps = {
+  image: "https://via.placeholder.com/345x140",
+  alt: "비어있는 이미지",
+  title: "프로젝트 제목",
+  description: "설명",
+  tags: ["개발", "React"],
+};
+
+export default ProductCard;
