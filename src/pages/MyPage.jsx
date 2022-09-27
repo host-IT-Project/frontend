37c8d48a1501a855a1696843c476444d94244abb
gohg0901 <gohg0901@naver.com>
2022-09-27 20:35:34 +0900

    초기

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import PageTemplate from '../template/PageTemplate';
 
-const MyPage = (props) => (
-    <PageTemplate>
-        <div>mypage</div>
-    </PageTemplate>
-);
+
+
+const MyPage = (props) => <PageTemplate> 
+    <div className="Card">
+            <div className="Card_img">
+                <img className="Dummy" alt="Dummy" src="https://via.placeholder.com/150" />
+            </div>
+            <div className='Card_info'>
+                Label
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                chip
+            </div>
+        </div>
+</PageTemplate>;
 
 export default MyPage;
