c5bf3604a14796cbe08e0586dabaf7d9df0e36ad
kmseonhwa <seonhwakei@gmail.com>
2022-10-01 22:32:42 +0900

    추가: 학식 메뉴 불러오기

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,33 @@
+import axios from 'axios';
 import React, { useEffect } from 'react';
+import { useRef } from 'react';
 import { getArticles } from '../api/articles';
 import PageTemplate from '../template/PageTemplate';
 
 const HomePage = (props) => {
+    const ref = useRef();
     useEffect(() => {
         (async function fetchData() {
             const articles = await getArticles({ page: 2, order: 'likes' });
             articles && console.log(articles);
         })();
+
+        axios.get('http://localhost:5001').then((table) => {
+            let WeeklyMeal = '';
+            table.data.weekly_meal.forEach((menu) => {
+                WeeklyMeal += `${menu}
+                `;
+            });
+            ref.current.innerHTML = WeeklyMeal;
+        });
     }, []);
 
-    return <PageTemplate>HomePage</PageTemplate>;
+    return (
+        <PageTemplate>
+            HomePage
+            <div ref={ref}></div>
+        </PageTemplate>
+    );
 };
 
 export default HomePage;
