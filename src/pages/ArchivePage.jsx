07153cccb1a7bc4142dff5841ffc6b27c349a1ec
kmseonhwa <seonhwakei@gmail.com>
2022-10-02 23:30:31 +0900

    #17 - 추가: ArchivePage에 카드리스트 출력

--- src/pages/ArchivePage.jsx
+++ src/pages/ArchivePage.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getArticles } from '../api/article';
+import ProductCardList from '../components/ProductCardList';
 import PageTemplate from '../template/PageTemplate';
 
-const ArchivePage = (props) => <PageTemplate>ArchivePage</PageTemplate>;
+const ArchivePage = (props) => {
+    const [articles, setArticles] = useState();
+
+    useEffect(() => {
+        (async function fetchData() {
+            const newArticles = await getArticles();
+            setArticles(newArticles.data);
+        })();
+    }, []);
+
+    return (
+        <PageTemplate>
+            {articles && <ProductCardList cardData={articles} />}
+        </PageTemplate>
+    );
+};
 
 export default ArchivePage;
