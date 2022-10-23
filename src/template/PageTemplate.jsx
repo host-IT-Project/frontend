c3c2fb6dc363d492db92d0007f5625478c8d4622
여다희 <yeodahui.dev@gmail.com>
2022-10-24 00:12:21 +0900

    수정: 페이지 템플릿 높이 제한 조정 및 ContentWrapper 추가

--- src/template/PageTemplate.jsx
+++ src/template/PageTemplate.jsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import Header from '../components/Header';
-import styled from 'styled-components';
+import React from "react";
+import Header from "../components/Header";
+import styled from "styled-components";
 
 const PageTemplateContainer = styled.div`
-    width: 100%;
-    height: 100%;
+  width: 100%;
+`;
+
+const ContentWrapper = styled.div`
+  max-width: 860px;
+  margin: 0 auto;
 `;
 
 const PageTemplate = ({ children }) => (
-    <PageTemplateContainer>
-        <Header />
-        {children}
-    </PageTemplateContainer>
+  <PageTemplateContainer>
+    <Header />
+    <ContentWrapper>{children}</ContentWrapper>
+  </PageTemplateContainer>
 );
 
 export default PageTemplate;
