71527054bfa164410708b4bf8ce169c15d54417a
kmseonhwa <seonhwakei@gmail.com>
2022-11-16 14:46:28 +0900

    추가: MainPage 구현
    - 작품 리스트의 배경 색상을 lightgray로 변경했습니다.
    - 작품 리스트 영역에는 필터링된 리스트를 보여줍니다.

--- src/components/ProductCardList.jsx
+++ src/components/ProductCardList.jsx
@@ -10,6 +10,7 @@ const ListItem = styled('li')(({ theme }) => ({
 const ProductCardList = ({ cardData }) => (
     <Paper
         elevation={0}
+        className={'ProductCardList-root'}
         sx={{
             display: 'flex',
             flexWrap: 'wrap',
@@ -17,12 +18,19 @@ const ProductCardList = ({ cardData }) => (
             listStyle: 'none',
             p: 0.5,
             m: 0,
+            background: 'rgb(249,249,249)',
         }}
         component="ul"
     >
         {cardData.map((data) => {
             return (
-                <ListItem key={data.id}>
+                <ListItem
+                    key={data.id}
+                    sx={{
+                        minWidth: '345px',
+                        height: '320px',
+                    }}
+                >
                     <ProductCard
                         image={data.image}
                         alt={data.alt}
@@ -59,6 +67,63 @@ ProductCardList.defaultProps = {
         },
         {
             id: '2',
+            image: 'https://via.placeholder.com/345x200',
+            alt: '비어있는 이미지',
+            title: '프로젝트 제목',
+            description: '설명',
+            tags: [
+                {
+                    key: 0,
+                    label: '태그1',
+                    href: '#',
+                },
+                {
+                    key: 1,
+                    label: '태그2',
+                    href: '#',
+                },
+            ],
+        },
+        {
+            id: '3',
+            image: 'https://via.placeholder.com/345x200',
+            alt: '비어있는 이미지',
+            title: '프로젝트 제목',
+            description: '설명',
+            tags: [
+                {
+                    key: 0,
+                    label: '태그1',
+                    href: '#',
+                },
+                {
+                    key: 1,
+                    label: '태그2',
+                    href: '#',
+                },
+            ],
+        },
+        {
+            id: '4',
+            image: 'https://via.placeholder.com/345x200',
+            alt: '비어있는 이미지',
+            title: '프로젝트 제목',
+            description: '설명',
+            tags: [
+                {
+                    key: 0,
+                    label: '태그1',
+                    href: '#',
+                },
+                {
+                    key: 1,
+                    label: '태그2',
+                    href: '#',
+                },
+            ],
+        },
+        {
+            id: '5',
             image: 'https://via.placeholder.com/345x200',
             alt: '비어있는 이미지',
             title: '프로젝트 제목',
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,67 @@
 import React from 'react';
+import styled from 'styled-components';
 import APITest from '../api/APITest';
 import BannerCarousel from '../components/BannerCarousel';
+import ProductCardList from '../components/ProductCardList';
+import SearchForm from '../components/search/SearchForm';
 import PageTemplate from '../template/PageTemplate';
 
 const HomePage = (props) => (
     <PageTemplate>
         <APITest />
         <BannerCarousel />
+        <StyledHomePage>
+            <SearchForm />
+            <div className="ScollList-container">
+                <h1 className={'title'}>지금 가장 조회수가 높은 👀</h1>
+                <ScrollList>
+                    <ProductCardList />
+                </ScrollList>
+            </div>
+            <div className="ScollList-container">
+                <h1 className={'title'}>가장 많이 사용된 스택 C#</h1>
+                <ScrollList>
+                    <ProductCardList />
+                </ScrollList>
+            </div>
+            <div className="ScollList-container">
+                <h1 className={'title'}>지금 막 올라왔어요 🆕</h1>
+                <ScrollList>
+                    <ProductCardList />
+                </ScrollList>
+            </div>
+        </StyledHomePage>
     </PageTemplate>
 );
+
+const StyledHomePage = styled.div`
+    width: 90%;
+    max-width: 1100px;
+    margin: 3rem auto;
+    .title {
+        font-size: 2.5em;
+        font-weight: bolder;
+        margin-left: 1.7rem;
+    }
+    .ScollList-container {
+        margin-bottom: 3rem;
+    }
+`;
+const ScrollList = styled.div`
+    height: 355px;
+    width: 100%;
+    overflow-x: scroll;
+    .ProductCardList-root {
+        flex-wrap: nowrap;
+        justify-content: flex-start;
+    }
+    /* Hide scrollbar for Chrome, Safari and Opera */
+    &::-webkit-scrollbar {
+        display: none;
+    }
+    /* Hide scrollbar for IE, Edge and Firefox */
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
+`;
 
 export default HomePage;
