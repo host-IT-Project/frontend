8e1a88944aa2dad7d19c600ce5fe15b2208252d1
여다희 <yeodahui.dev@gmail.com>
2022-11-18 00:45:03 +0900

    추가: 프로젝트 상세 페이지 Article 데이터 fetch

--- src/pages/ProjectDetailPage.jsx
+++ src/pages/ProjectDetailPage.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
-import PageTemplate from '../template/PageTemplate';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getArticles } from "../api/article";
+import ProjectInfoBox from "../components/ProjectInfoBox";
+import PageTemplate from "../template/PageTemplate";
 
-const ProjectDetailPage = (props) => (
-    <PageTemplate>ProjectDetailPage</PageTemplate>
-);
+const ProjectDetailPage = (props) => {
+  let { id } = useParams();
+  const [article, setArticle] = useState();
+
+  useEffect(() => {
+    (async function fetchData() {
+      const articles = await getArticles();
+      setArticle(articles[id]);
+    })();
+    console.log(article);
+  }, [article, id]);
+
+  return (
+    <PageTemplate>
+      <div>
+        <ProjectInfoBox />
+      </div>
+    </PageTemplate>
+  );
+};
 
 export default ProjectDetailPage;
