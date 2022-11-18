f33f531f758598118b684a08b3412304ca8ebc7a
여다희 <yeodahui.dev@gmail.com>
2022-11-18 15:37:52 +0900

    스타일: style.js의 colors를 theme.js의 colors에 병합

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,22 +1,31 @@
 const theme = {
-    light: {
-        id: 'T_001',
-        name: 'Light',
-        colors: {
-            body: '#fff',
-            text: '#000',
-            background: 'rgb(249,249,249)',
-            button: {
-                text: '#FFFFFF',
-                background: '#000000',
-            },
-            link: {
-                text: 'teal',
-                opacity: 1,
-            },
-        },
-        font: 'SpoqaHanSansNeo, system-ui, -apple-system, BlinkMacSystemFont, Roboto, sans-serif;',
+  light: {
+    id: "T_001",
+    name: "Light",
+    colors: {
+      body: "#fff",
+      text: "#222",
+      background: "rgb(249,249,249)",
+      button: {
+        text: "#FFFFFF",
+        background: "#000000",
+      },
+      link: {
+        text: "teal",
+        opacity: 1,
+      },
+      primary: "#3579F6",
+      secondary: "#9aaae6",
+      error: "#eb5757",
+      black: "#000",
+      darkgray: "#767676",
+      middlegray: "#c4c4c4",
+      lightgray: "#dbdbdb",
+      whitegray: "#f2f2f2",
+      white: "#fff",
     },
+    font: "SpoqaHanSansNeo, system-ui, -apple-system, BlinkMacSystemFont, Roboto, sans-serif;",
+  },
 };
 
 /**
