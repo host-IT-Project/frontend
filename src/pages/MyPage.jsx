73ef27b5e86dd6db3565cbecfaf1692ded0a2b6c
여다희 <yeodahui.dev@gmail.com>
2022-11-20 04:53:23 +0900

    수정: 마이페이지에서 카드 가로형 미리보기

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,38 +1,10 @@
-import React from 'react';
-import { patchArticle } from '../api/article';
-import PageTemplate from '../template/PageTemplate';
+import React from "react";
+import MoreMenu from "../components/MoreMenu";
+import ProductCardHorizontal from "../components/ProductCardHoriz";
+import PageTemplate from "../template/PageTemplate";
 
 const MyPage = (props) => {
-  return (
-    <PageTemplate>
-      <div
-        className="Card"
-        // api test
-        onClick={() => {
-          (async () => {
-            await patchArticle('2', {
-              title: '잘래',
-              content: 'ㅠㅠ',
-              articleCategory: '질문',
-              hashtagList: ['chicken', 'pizza'],
-            });
-          })();
-        }}
-      >
-        <div className="Card_img">
-          <img
-            className="Dummy"
-            alt="Dummy"
-            src="https://via.placeholder.com/150"
-          />
-        </div>
-        <div className="Card_info">
-          Label It is a long established fact that a reader will be distracted
-          by the readable content of a page when looking at its layout. chip
-        </div>
-      </div>
-    </PageTemplate>
-  );
+  return <PageTemplate contents={<ProductCardHorizontal />}></PageTemplate>;
 };
 
 export default MyPage;
