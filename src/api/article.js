089d46efcd5e0c7514dddafa416387e21732b6ac
kmseonhwa <seonhwakei@gmail.com>
2022-11-19 14:09:28 +0900

    WIP: 소셜로그인 구현

--- src/api/article.js
+++ src/api/article.js
@@ -9,23 +9,23 @@ import { http } from './http';
  * @param {Array<string>} [hashtag]
  */
 export const getArticles = ({ page, order, keyword, hashtag } = {}) => {
-    page && checkType(page, 'number');
-    order && checkType(order, 'string');
-    keyword && checkType(keyword, 'string');
-    hashtag && checkType(hashtag, 'object');
-    return http
-        .get(`/api/board/1/articles`, {
-            page,
-            order,
-            keyword,
-            hashtag,
-        })
-        .catch((error) => {
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-            return cardDemoData;
-        });
+  page && checkType(page, 'number');
+  order && checkType(order, 'string');
+  keyword && checkType(keyword, 'string');
+  hashtag && checkType(hashtag, 'object');
+  return http
+    .get(`/api/board/1/articles`, {
+      page,
+      order,
+      keyword,
+      hashtag,
+    })
+    .catch((error) => {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      return cardDemoData;
+    });
 };
 
 /**
@@ -36,28 +36,28 @@ export const getArticles = ({ page, order, keyword, hashtag } = {}) => {
  * @param {Array<string>} [hashtagList]
  */
 export const postArticle = ({
-    title,
-    content,
-    articleCategory,
-    hashtagList,
+  title,
+  content,
+  articleCategory,
+  hashtagList,
 } = {}) => {
-    title && checkType(title, 'string');
-    content && checkType(content, 'string');
-    articleCategory && checkType(articleCategory, 'string');
-    hashtagList && checkType(hashtagList, 'object');
-    return http
-        .post(`/api/board/1/articles`, {
-            title,
-            content,
-            articleCategory,
-            hashtagList,
-        })
-        .catch((error) => {
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-            window.alert('게시물 등록에 실패했습니다.');
-        });
+  title && checkType(title, 'string');
+  content && checkType(content, 'string');
+  articleCategory && checkType(articleCategory, 'string');
+  hashtagList && checkType(hashtagList, 'object');
+  return http
+    .post(`/api/board/1/articles`, {
+      title,
+      content,
+      articleCategory,
+      hashtagList,
+    })
+    .catch((error) => {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      window.alert('게시물 등록에 실패했습니다.');
+    });
 };
 
 /**
@@ -65,15 +65,15 @@ export const postArticle = ({
  * @param {string} articleId
  */
 export const deleteArticle = (articleId) => {
-    articleId && checkType(articleId, 'string');
-    return http
-        .delete(`/api/article/${articleId}`) //
-        .catch((error) => {
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-            window.alert('게시물 삭제에 실패했습니다.');
-        });
+  articleId && checkType(articleId, 'string');
+  return http
+    .delete(`/api/article/${articleId}`) //
+    .catch((error) => {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      window.alert('게시물 삭제에 실패했습니다.');
+    });
 };
 
 /**
@@ -85,25 +85,25 @@ export const deleteArticle = (articleId) => {
  * @param {Array<string>} [hashtagList]
  */
 export const patchArticle = (
-    articleId,
-    { title, content, articleCategory, hashtagList } = {}
+  articleId,
+  { title, content, articleCategory, hashtagList } = {}
 ) => {
-    articleId && checkType(articleId, 'string');
-    title && checkType(title, 'string');
-    content && checkType(content, 'string');
-    articleCategory && checkType(articleCategory, 'string');
-    hashtagList && checkType(hashtagList, 'object');
-    return http
-        .patch(`/api/article/${articleId}`, {
-            title,
-            content,
-            articleCategory,
-            hashtagList,
-        })
-        .catch((error) => {
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-            window.alert('게시물 수정에 실패했습니다.');
-        });
+  articleId && checkType(articleId, 'string');
+  title && checkType(title, 'string');
+  content && checkType(content, 'string');
+  articleCategory && checkType(articleCategory, 'string');
+  hashtagList && checkType(hashtagList, 'object');
+  return http
+    .patch(`/api/article/${articleId}`, {
+      title,
+      content,
+      articleCategory,
+      hashtagList,
+    })
+    .catch((error) => {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      window.alert('게시물 수정에 실패했습니다.');
+    });
 };
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -9,104 +9,123 @@ import PageTemplate from '../template/PageTemplate';
 import { Button } from '@mui/material';
 
 const LoginPage = (props) => {
-    return (
-        <PageTemplate>
-            <StyledLoginPage>
-                <div className={'LoginButton-container'}>
-                    <img className={'logo'} src={logo} alt="호잇" />
-                    <LoginButton className={'kakaoLogin'}>
-                        <img
-                            className={'login'}
-                            src={kakaoLogin}
-                            alt="카카오로그인"
-                        />
-                        <span>카카오톡 계정으로 로그인</span>
-                    </LoginButton>
-                    <LoginButton className={'naverLogin'}>
-                        <img
-                            className={'login'}
-                            src={naverLogin}
-                            alt="네이버로그인"
-                        />
-                        <span>네이버 계정으로 로그인</span>
-                    </LoginButton>
-                    <LoginButton className={'googleLogin'}>
-                        <img
-                            className={'login'}
-                            src={googleLogin}
-                            alt="구글로그인"
-                        />
-                        <span>구글 계정으로 로그인</span>
-                    </LoginButton>
-                </div>
-            </StyledLoginPage>
-        </PageTemplate>
-    );
+  const socialType = 'kakao';
+  // const REDIRECT_URI = `http://localhost:3000/oauth/redirect`;
+  // const REDIRECT_URI = `${process.env.REACT_APP_BASE_URL}/oauth/redirect`;
+  // const REDIRECT_URI = `https://www.spadeworker.site/oauth/redirect`;
+
+  const BACKEND_URL = `https://spadeworker.site`;
+  const FRONTEND_PORT = 3000;
+  // process.env.FRONTEND_PORT === null ? '' : `:${process.env.FRONTEND_PORT}`;
+  const REDIRECT_URI = `${window.location.protocol}//${window.location.hostname}:${FRONTEND_PORT}/oauth/redirect`;
+
+  return (
+    <PageTemplate>
+      <StyledLoginPage>
+        <div className={'LoginButton-container'}>
+          <img className={'logo'} src={logo} alt="호잇" />
+          <LoginButton
+            className={'kakaoLogin'}
+            // onClick={(e) => {
+            //   axios.post(
+            //     `/api/v1/auth`,
+            //     {
+            //       id: 'abc',
+            //       password: ' abc',
+            //     }
+            //     // appendAuth(config)
+            //   );
+            //   // .then(handler.handle(success))
+            //   // .catch(fail)
+            // }}
+          >
+            <a
+              href={
+                // `${process.env.REACT_APP_BASE_URL}/oauth2/authorization/${socialType}?redirect_uri=${REDIRECT_URI}`
+                `${BACKEND_URL}/oauth2/authorization/${socialType}?redirect_uri=${REDIRECT_URI}`
+              }
+            >
+              <img className={'login'} src={kakaoLogin} alt="카카오로그인" />
+              <span>카카오톡 계정으로 로그인</span>
+            </a>
+          </LoginButton>
+          <LoginButton className={'naverLogin'}>
+            <img className={'login'} src={naverLogin} alt="네이버로그인" />
+            <span>네이버 계정으로 로그인</span>
+          </LoginButton>
+          <LoginButton className={'googleLogin'}>
+            <img className={'login'} src={googleLogin} alt="구글로그인" />
+            <span>구글 계정으로 로그인</span>
+          </LoginButton>
+        </div>
+      </StyledLoginPage>
+    </PageTemplate>
+  );
 };
 
 const StyledLoginPage = styled.div`
+  width: 100%;
+  height: 100%;
+  div.LoginButton-container {
+    border-radius: 1rem;
+    background: white;
     width: 100%;
-    height: 100%;
-    div.LoginButton-container {
-        border-radius: 1rem;
-        background: white;
-        width: 100%;
-        max-width: 40rem;
-        height: 60vh;
-        margin: 3rem auto 0 auto;
-        box-shadow: 0 2px 4px 0 hsl(0deg 0% 81% / 50%);
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-    }
-    img.logo {
-        width: 7rem;
-        margin-bottom: 5rem;
-    }
+    max-width: 40rem;
+    height: 60vh;
+    margin: 3rem auto 0 auto;
+    box-shadow: 0 2px 4px 0 hsl(0deg 0% 81% / 50%);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+  }
+  img.logo {
+    width: 7rem;
+    margin-bottom: 5rem;
+  }
 `;
 
 const LoginButton = styled(Button)`
+  width: 100%;
+  max-width: 30rem;
+  max-height: 70px;
+  padding: 0px;
+  border: none;
+  border-radius: 0.5rem;
+  background: transparent;
+  margin-bottom: 2rem;
+  display: flex;
+  align-items: center;
+  box-shadow: 1px 2px 9px 1px hsl(0deg 0% 81% / 50%);
+  img.login {
+    width: 28px;
+    height: 28px;
+    margin-left: 1rem;
+  }
+  span {
+    margin: 0 auto;
+    font-size: 1.5rem;
+  }
+  &.MuiButton-text {
+    color: black;
+  }
+  &.kakaoLogin {
+    background-color: #fee500;
     width: 100%;
-    max-width: 30rem;
-    max-height: 70px;
-    padding: 0px;
-    border: none;
-    border-radius: 0.5rem;
-    background: transparent;
-    margin-bottom: 2rem;
-    display: flex;
-    align-items: center;
-    box-shadow: 1px 2px 9px 1px hsl(0deg 0% 81% / 50%);
-    img.login {
-        width: 28px;
-        height: 28px;
-        margin-left: 1rem;
-    }
-    span {
-        margin: 0 auto;
-        font-size: 1.5rem;
-    }
-    &.MuiButton-text {
-        color: black;
-    }
-    &.kakaoLogin {
-        background-color: #fee500;
-        width: 100%;
-        height: 50px;
-    }
-    &.naverLogin {
-        background-color: #02c75a;
-        color: white;
-        width: 100%;
-        height: 50px;
-    }
-    &.googleLogin {
-        width: 100%;
-        height: 50px;
-        color: #827c79;
-    }
+    height: 50px;
+  }
+  &.naverLogin {
+    background-color: #02c75a;
+    color: white;
+    width: 100%;
+    height: 50px;
+  }
+  &.googleLogin {
+    width: 100%;
+    height: 50px;
+    color: #827c79;
+  }
 `;
 
 export default LoginPage;
