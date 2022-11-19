71625c5c2e4d8da4fc33bbe54371d54d4a9ed84f
kmseonhwa <seonhwakei@gmail.com>
2022-11-20 04:17:18 +0900

    추가: 로그인 사용자 한정 마이페이지 접근

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 import { patchArticle } from '../api/article';
+import { userSelector } from '../atom/userAtom';
 import PageTemplate from '../template/PageTemplate';
 
 const MyPage = (props) => {
+  const user = useRecoilValue(userSelector);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user.isLogin) {
+      navigate('/login');
+    }
+  }, []);
+
   return (
     <PageTemplate>
       <div
