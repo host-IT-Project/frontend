7129de04dafd8fa2bcfa3e338ab7fd657e8a8849
kmseonhwa <seonhwakei@gmail.com>
2022-11-19 16:47:17 +0900

    추가: 임시 사용자 상태 관리 추가

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,18 @@ import { GlobalStyles } from './styles/global';
 import theme from './styles/theme';
 
 function App() {
-    return (
-        <RecoilRoot>
-            <Suspense fallback={<div>Loading...</div>}>
-                <ThemeProvider theme={theme.light}>
-                    <GlobalStyles />
-                    <div className="App">
-                        <Routes />
-                    </div>
-                </ThemeProvider>
-            </Suspense>
-        </RecoilRoot>
-    );
+  return (
+    <RecoilRoot>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ThemeProvider theme={theme.light}>
+          <GlobalStyles />
+          <div className="App">
+            <Routes />
+          </div>
+        </ThemeProvider>
+      </Suspense>
+    </RecoilRoot>
+  );
 }
 
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,64 +8,64 @@ import SearchForm from '../components/search/SearchForm';
 import PageTemplate from '../template/PageTemplate';
 
 const HomePage = (props) => {
-    const articles = useRecoilValue(articleSelector);
+  const articles = useRecoilValue(articleSelector);
 
-    return (
-        <PageTemplate
-            contents={
-                <StyledHomePage>
-                    <SearchForm />
-                    <div className="ScollList-container">
-                        <h1 className={'title'}>지금 가장 조회수가 높은 👀</h1>
-                        <ScrollList>
-                            <ProductCardList cardData={articles} />
-                        </ScrollList>
-                    </div>
-                    <div className="ScollList-container">
-                        <h1 className={'title'}>가장 많이 사용된 스택 C#</h1>
-                        <ScrollList>
-                            <ProductCardList cardData={articles} />
-                        </ScrollList>
-                    </div>
-                    <div className="ScollList-container">
-                        <h1 className={'title'}>지금 막 올라왔어요 🆕</h1>
-                        <ScrollList>
-                            <ProductCardList cardData={articles} />
-                        </ScrollList>
-                    </div>
-                </StyledHomePage>
-            }
-        >
-            <BannerCarousel items={itemsForHomePage} />
-        </PageTemplate>
-    );
+  return (
+    <PageTemplate
+      contents={
+        <StyledHomePage>
+          <SearchForm />
+          <div className="ScollList-container">
+            <h1 className={'title'}>지금 가장 조회수가 높은 👀</h1>
+            <ScrollList>
+              <ProductCardList cardData={articles} />
+            </ScrollList>
+          </div>
+          <div className="ScollList-container">
+            <h1 className={'title'}>가장 많이 사용된 스택 C#</h1>
+            <ScrollList>
+              <ProductCardList cardData={articles} />
+            </ScrollList>
+          </div>
+          <div className="ScollList-container">
+            <h1 className={'title'}>지금 막 올라왔어요 🆕</h1>
+            <ScrollList>
+              <ProductCardList cardData={articles} />
+            </ScrollList>
+          </div>
+        </StyledHomePage>
+      }
+    >
+      <BannerCarousel items={itemsForHomePage} />
+    </PageTemplate>
+  );
 };
 
 const StyledHomePage = styled.div`
-    .title {
-        font-size: 2.5em;
-        font-weight: bolder;
-        margin-left: 1.7rem;
-    }
-    .ScollList-container {
-        margin-bottom: 3rem;
-    }
+  .title {
+    font-size: 2.5em;
+    font-weight: bolder;
+    margin-left: 1.7rem;
+  }
+  .ScollList-container {
+    margin-bottom: 3rem;
+  }
 `;
 const ScrollList = styled.div`
-    height: 355px;
-    width: 100%;
-    overflow-x: scroll;
-    .ProductCardList-root {
-        flex-wrap: nowrap;
-        justify-content: flex-start;
-    }
-    /* Hide scrollbar for Chrome, Safari and Opera */
-    &::-webkit-scrollbar {
-        display: none;
-    }
-    /* Hide scrollbar for IE, Edge and Firefox */
-    -ms-overflow-style: none; /* IE and Edge */
-    scrollbar-width: none; /* Firefox */
+  height: 355px;
+  width: 100%;
+  overflow-x: scroll;
+  .ProductCardList-root {
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+  }
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  /* Hide scrollbar for IE, Edge and Firefox */
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
 `;
 
 export default HomePage;
