0539f3082e5695cc902691c2a73fc5f95ccee1f0
kmseonhwa <seonhwakei@gmail.com>
2022-11-20 03:31:39 +0900

    추가: ProjectDetailPage 내부의 article state를 recoil state로 변경

--- src/components/ProductCardList.jsx
+++ src/components/ProductCardList.jsx
@@ -4,141 +4,141 @@ import Paper from '@mui/material/Paper';
 import ProductCard from './ProductCard';
 
 const ListItem = styled('li')(({ theme }) => ({
-    margin: theme.spacing(0.8),
+  margin: theme.spacing(0.8),
 }));
 
 export const cardDemoData = [
-    {
-        id: 24,
-        title: '게시글1 제목입니다!!',
-        content: '게겍게겍ㄲ',
-        likesCount: 100,
-        dislikesCount: 0,
-        articleCategory: '잡담',
-        createdAt: '2022-11-16T10:19:03',
-        user: {
-            id: 1,
-            username: '게시글 작성자',
-            profileImgUrl: 'tester photo',
-        },
-        hashtagList: [],
+  {
+    id: 24,
+    title: '게시글1 제목입니다!!',
+    content: '게겍게겍ㄲ',
+    likesCount: 100,
+    dislikesCount: 0,
+    articleCategory: '잡담',
+    createdAt: '2022-11-16T10:19:03',
+    user: {
+      id: 1,
+      username: '게시글 작성자',
+      profileImgUrl: 'tester photo',
     },
-    {
-        id: 3,
-        title: '게시글2 제목입니다!!',
-        content: '게겍게겍ㄲ',
-        likesCount: 100,
-        dislikesCount: 0,
-        articleCategory: '잡담',
-        createdAt: '2022-10-14T05:21:13',
-        user: {
-            id: 1,
-            username: '게시글 작성자',
-            profileImgUrl: 'tester photo',
-        },
-        hashtagList: [],
+    hashtagList: [],
+  },
+  {
+    id: 3,
+    title: '게시글2 제목입니다!!',
+    content: '게겍게겍ㄲ',
+    likesCount: 100,
+    dislikesCount: 0,
+    articleCategory: '잡담',
+    createdAt: '2022-10-14T05:21:13',
+    user: {
+      id: 1,
+      username: '게시글 작성자',
+      profileImgUrl: 'tester photo',
     },
-    {
-        id: 19,
-        title: '게시글3 제목입니다!!',
-        content: '게겍게겍ㄲ',
-        likesCount: 100,
-        dislikesCount: 0,
-        articleCategory: '잡담',
-        createdAt: '2022-11-16T10:17:38',
-        user: {
-            id: 1,
-            username: '게시글 작성자',
-            profileImgUrl: 'tester photo',
-        },
-        hashtagList: [],
+    hashtagList: [],
+  },
+  {
+    id: 19,
+    title: '게시글3 제목입니다!!',
+    content: '게겍게겍ㄲ',
+    likesCount: 100,
+    dislikesCount: 0,
+    articleCategory: '잡담',
+    createdAt: '2022-11-16T10:17:38',
+    user: {
+      id: 1,
+      username: '게시글 작성자',
+      profileImgUrl: 'tester photo',
     },
-    {
-        id: 14,
-        title: '게시글4 제목입니다!!',
-        content: '게겍게겍ㄲ',
-        likesCount: 100,
-        dislikesCount: 0,
-        articleCategory: '잡담',
-        createdAt: '2022-11-16T09:42:30',
-        user: {
-            id: 1,
-            username: '게시글 작성자',
-            profileImgUrl: 'tester photo',
-        },
-        hashtagList: [],
+    hashtagList: [],
+  },
+  {
+    id: 14,
+    title: '게시글4 제목입니다!!',
+    content: '게겍게겍ㄲ',
+    likesCount: 100,
+    dislikesCount: 0,
+    articleCategory: '잡담',
+    createdAt: '2022-11-16T09:42:30',
+    user: {
+      id: 1,
+      username: '게시글 작성자',
+      profileImgUrl: 'tester photo',
     },
-    {
-        id: 9,
-        title: '게시글5 제목입니다!!',
-        content: '게겍게겍ㄲ',
-        likesCount: 100,
-        dislikesCount: 0,
-        articleCategory: '잡담',
-        createdAt: '2022-11-04T11:25:27',
-        user: {
-            id: 1,
-            username: '게시글 작성자',
-            profileImgUrl: 'tester photo',
-        },
-        hashtagList: [],
+    hashtagList: [],
+  },
+  {
+    id: 9,
+    title: '게시글5 제목입니다!!',
+    content: '게겍게겍ㄲ',
+    likesCount: 100,
+    dislikesCount: 0,
+    articleCategory: '잡담',
+    createdAt: '2022-11-04T11:25:27',
+    user: {
+      id: 1,
+      username: '게시글 작성자',
+      profileImgUrl: 'tester photo',
     },
-    {
-        id: 26,
-        title: '게시글 제목입니다6!!',
-        content: '게겍게겍ㄲ',
-        likesCount: 54,
-        dislikesCount: 0,
-        articleCategory: '잡담',
-        createdAt: '2022-11-16T10:19:03',
-        user: {
-            id: 1,
-            username: '게시글 작성자',
-            profileImgUrl: 'tester photo',
-        },
-        hashtagList: [],
+    hashtagList: [],
+  },
+  {
+    id: 26,
+    title: '게시글 제목입니다6!!',
+    content: '게겍게겍ㄲ',
+    likesCount: 54,
+    dislikesCount: 0,
+    articleCategory: '잡담',
+    createdAt: '2022-11-16T10:19:03',
+    user: {
+      id: 1,
+      username: '게시글 작성자',
+      profileImgUrl: 'tester photo',
     },
+    hashtagList: [],
+  },
 ];
 
 const ProductCardList = ({ cardData }) => (
-    <Paper
-        elevation={0}
-        className={'ProductCardList-root'}
-        sx={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            listStyle: 'none',
-            p: 0.5,
-            m: 0,
-            background: 'rgb(249,249,249)',
-        }}
-        component="ul"
-    >
-        {cardData.map((data) => {
-            return (
-                <ListItem
-                    key={data.id}
-                    sx={{
-                        minWidth: '345px',
-                        height: '320px',
-                    }}
-                >
-                    <ProductCard
-                        image={data.image}
-                        alt={data.alt}
-                        title={data.title}
-                        description={data.description}
-                        tags={data.tags}
-                    />
-                </ListItem>
-            );
-        })}
-    </Paper>
+  <Paper
+    elevation={0}
+    className={'ProductCardList-root'}
+    sx={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+      listStyle: 'none',
+      p: 0.5,
+      m: 0,
+      background: 'rgb(249,249,249)',
+    }}
+    component="ul"
+  >
+    {cardData.map((data) => {
+      return (
+        <ListItem
+          key={data.id}
+          sx={{
+            minWidth: '345px',
+            height: '320px',
+          }}
+        >
+          <ProductCard
+            image={data.image}
+            alt={data.alt}
+            title={data.title}
+            description={data.description}
+            tags={data.tags}
+          />
+        </ListItem>
+      );
+    })}
+  </Paper>
 );
 
 ProductCardList.defaultProps = {
-    cardData: cardDemoData,
+  cardData: cardDemoData,
 };
 
 export default ProductCardList;
