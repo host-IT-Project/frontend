ea78ad180f10ea2e0ae5744093e15ca9278cf855
여다희 <yeodahui.dev@gmail.com>
2022-11-21 05:55:44 +0900

    스타일: 새 글 쓰기 버튼 고정 위치 수정

--- src/components/NewArticleButton.jsx
+++ src/components/NewArticleButton.jsx
@@ -8,12 +8,13 @@ const StyledButton = styled(Button)`
   margin-left: auto;
   width: fit-content;
   position: fixed;
-  right: 10px;
-  bottom: 10px;
+  right: 20px;
+  bottom: 20px;
   left: 10px;
   z-index: 99999;
   border-radius: 100px;
   font-size: 1.6rem;
+  border: 0;
   background-color: ${({ theme }) => theme.colors.primary};
 `;
 
