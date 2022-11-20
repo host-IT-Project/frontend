18d43c2c370fcce12eb72b17adc466d18ced6c53
kmseonhwa <seonhwakei@gmail.com>
2022-11-21 06:06:08 +0900

    수정: React.Suspense를 Spinner로 수정

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './reset.css';
-import './global.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { StyledEngineProvider } from '@mui/styled-engine';
-import { Suspense } from 'react';
-import { RecoilRoot } from 'recoil';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./reset.css";
+import "./global.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { StyledEngineProvider } from "@mui/styled-engine";
+import { Suspense } from "react";
+import { RecoilRoot } from "recoil";
+import Spinner from "./components/Spinner";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <StyledEngineProvider injectFirst>
         <RecoilRoot>
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<Spinner />}>
             <App />
           </Suspense>
         </RecoilRoot>
