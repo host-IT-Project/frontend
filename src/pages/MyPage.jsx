ea5cbb6946f0047e7a681c28a2e765ce0aa22309
여다희 <yeodahui.dev@gmail.com>
2022-11-20 16:14:42 +0900

    추가 : MyPage UI 구성

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
-import { patchArticle } from '../api/article';
-import { userSelector } from '../atom/userAtom';
-import PageTemplate from '../template/PageTemplate';
-import ProductCardHorizontal from "../components/ProductCardHoriz";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { userSelector } from "../atom/userAtom";
+import PageTemplate from "../template/PageTemplate";
+import ProductCardList from "../components/ProductCardList";
+import styled from "styled-components";
 
 const MyPage = (props) => {
   const user = useRecoilValue(userSelector);
@@ -12,13 +12,28 @@ const MyPage = (props) => {
 
   useEffect(() => {
     if (!user.isLogin) {
-      navigate('/login');
+      navigate("/login");
     }
   }, []);
 
   return (
-    <PageTemplate contents={<ProductCardHorizontal />}></PageTemplate>
+    <PageTemplate
+      contents={
+        <>
+          <StyledHeading>My Porfolio</StyledHeading>
+          <ProductCardList horiz={true} />
+        </>
+      }
+    ></PageTemplate>
   );
 };
+
+const StyledHeading = styled.h1`
+  padding: 10px;
+  margin-bottom: 20px;
+  font-size: 2.4rem;
+  text-align: center;
+  font-family: ${({ theme }) => theme.font.gmarketSans};
+`;
 
 export default MyPage;
