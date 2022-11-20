e304b14c164c5bd8e2ab6b652a47cca8b91512b7
여다희 <yeodahui.dev@gmail.com>
2022-11-20 20:31:50 +0900

    수정: 게시글 불러올 때 아이디 prop으로 넘기지 않으면 Return

--- src/atom/articleAtom.js
+++ src/atom/articleAtom.js
@@ -1,7 +1,7 @@
-import { atom, selector, selectorFamily } from 'recoil';
-import { v1 } from 'uuid';
-import { getArticle, getArticles } from '../api/article';
-import { cardDemoData } from '../components/ProductCardList';
+import { atom, selector, selectorFamily } from "recoil";
+import { v1 } from "uuid";
+import { getArticle, getArticles } from "../api/article";
+import { cardDemoData } from "../components/ProductCardList";
 
 export const articleAtom = atom({
   key: `articleAtom/${v1()}`,
@@ -58,23 +58,25 @@ export const selectedArticleSelector = selectorFamily({
       //   selectedArticle = article;
       // }
       // return selectedArticle;
-
+      if (!articleId) {
+        return;
+      }
       const { data } = await getArticle(articleId.toString());
       if (data) {
         return data.article;
       } else {
         return {
           id: 1,
-          title: '게시글1 제목입니다!!',
-          content: '게겍게겍ㄲ',
+          title: "게시글1 제목입니다!!",
+          content: "게겍게겍ㄲ",
           likesCount: 10,
           dislikesCount: 0,
-          articleCategory: '잡담',
-          createdAt: '2022-10-14T05:21:13',
+          articleCategory: "잡담",
+          createdAt: "2022-10-14T05:21:13",
           user: {
             id: 1,
-            username: '게시글 작성자',
-            profileImgUrl: 'tester photo',
+            username: "게시글 작성자",
+            profileImgUrl: "tester photo",
           },
           hashtagList: [],
         };
