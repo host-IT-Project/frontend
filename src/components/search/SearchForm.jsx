7ba1ffdfa85dba12c1534784240a68a773bb028b
kmseonhwa <seonhwakei@gmail.com>
2022-11-21 13:20:11 +0900

    수정: TagSearch 내림

--- src/components/search/SearchForm.jsx
+++ src/components/search/SearchForm.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import styled from 'styled-components';
-import TagSearch from './TagSearch';
-import TitleSearch from './TitleSearch';
+import React from "react";
+import styled from "styled-components";
+import TagSearch from "./TagSearch";
+import TitleSearch from "./TitleSearch";
 
 const SearchForm = ({
-    articles = [],
-    tagList = [],
-    onSubmitInput,
-    onSubmitTag,
+  articles = [],
+  tagList = [],
+  onSubmitInput,
+  onSubmitTag,
 }) => {
-    return (
-        <SearchFormContainer>
-            <TitleSearch onSubmitInput={onSubmitInput} />
-            <TagSearch tagList={tagList} onSubmitTag={onSubmitTag} />
-        </SearchFormContainer>
-    );
+  return (
+    <SearchFormContainer>
+      <TitleSearch onSubmitInput={onSubmitInput} />
+      {/* <TagSearch tagList={tagList} onSubmitTag={onSubmitTag} /> */}
+    </SearchFormContainer>
+  );
 };
 
 const SearchFormContainer = styled.div`
-    margin: 30px auto;
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  margin: 30px auto;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export default SearchForm;
