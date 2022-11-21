9d682f5c65ef85c0f33243b6213cc931e56c959f
여다희 <yeodahui.dev@gmail.com>
2022-11-21 10:46:20 +0900

    추가: 개인정보 처리약관 페이지 생성 및 라우트

--- src/pages/Routes.jsx
+++ src/pages/Routes.jsx
@@ -9,6 +9,7 @@ import LoginPage from "./LoginPage";
 import LogoutPage from "./LogoutPage";
 import RedirectLogin from "../components/login/RedirectLogin";
 import ErrorPage from "./ErrorPage";
+import PolicyPage from "./PolicyPage";
 
 const Routes = () => {
   return (
@@ -22,6 +23,7 @@ const Routes = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/logout" element={<LogoutPage />} />
       <Route path="/error" element={<ErrorPage />} />
+      <Route path="/policy" element={<PolicyPage />} />
       <Route path="/oauth/redirect" element={<RedirectLogin />} />
       <Route path="*" element={<Navigate replace to="/home" />} />
     </ReactRouterRoutes>
