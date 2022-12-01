4e374022685b413e0e611b3ea9a5812a946be05e
여다희 <yeodahui.dev@gmail.com>
2022-12-01 20:14:48 +0900

    수정: SearchForm의 submit 핸들러를 TitleSearch 컴포넌트로 이동

--- src/components/search/SearchForm.jsx
+++ src/components/search/SearchForm.jsx
@@ -1,26 +1,11 @@
 import React from "react";
-import { Navigate, useLocation } from "react-router-dom";
-import { useSetRecoilState } from "recoil";
-import { articlesFilterState } from "../../atom/articleAtom";
 import { SearchFormContainer } from "./SearchFormStyle";
 import TitleSearch from "./TitleSearch";
 
 const SearchForm = (props) => {
-  const setFilter = useSetRecoilState(articlesFilterState);
-  const location = useLocation();
-
-  const handleSubmitInput = (e) => {
-    const value = e.target.value;
-    setFilter(value.trim());
-
-    if (location === "/home") {
-      Navigate("/archive");
-    }
-  };
-
   return (
     <SearchFormContainer>
-      <TitleSearch onSubmitInput={handleSubmitInput} />
+      <TitleSearch />
       {/* <TagSearch tagList={tagList} onSubmitTag={onSubmitTag} /> */}
     </SearchFormContainer>
   );
